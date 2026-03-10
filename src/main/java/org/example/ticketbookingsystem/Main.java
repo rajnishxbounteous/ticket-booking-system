1b7c686c6a041e4d82956d26f06b826a62a7d15c
Rajnish Kumar <rajnish.kumar@bounteous.com>
2026-03-10 15:42:29 +0530

    Modified main method to call the BookingSimulation

--- src/main/java/org/example/ticketbookingsystem/Main.java
+++ src/main/java/org/example/ticketbookingsystem/Main.java
@@ -1,7 +1,16 @@
 package org.example.ticketbookingsystem;
 
+import org.example.ticketbookingsystem.ticketbooking.BookingSimulation;
+
+/**
+ * Main class is now just an entry point.
+ * It delegates the actual simulation to BookingSimulation.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ticket Booking System...");
+        BookingSimulation simulation = new BookingSimulation();
+        simulation.runSimulation(50, 100, 10);
+        // 50 tickets, 100 users, 10 threads
     }
 }
+
